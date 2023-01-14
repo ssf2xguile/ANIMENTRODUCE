d316b7796b60678246269fd955a3dcb74e0ec3eb
ssf2xguile <iwanatteiwanai123@gmail.com>
2023-01-14 22:09:03 +0900

    ネストが深くなる問題をちょっと解決した。

--- linebot/message_create.py
+++ linebot/message_create.py
@@ -5,62 +5,52 @@ import json
 import pprint
 
 def create_single_text_message(message):
-    msg_array = message.split('-')  # メッセージを分割 例：ANIMENTRODUCE-今期アニメ または ANIMENTRODUCE-今期アニメ-アニメ名
-    first_argument = msg_array[0]  # ANIMENTRODUCE、ヘルプ
+    cool = {1: '冬アニメ', 2: '春アニメ', 3: '夏アニメ', 4: '秋アニメ'}
+    msg_array = message.split('-')  # メッセージを分割 例：今期アニメ または 今期アニメ-アニメ名
+    first_argument = msg_array[0]  # 今期アニメ 、来期アニメ 、前期アニメ 、ヘルプ
     try:
-        second_argument = msg_array[1]  # 今期アニメ、来期アニメ、前期アニメ
+        second_argument = msg_array[1]  # アニメ名
     except IndexError:
         second_argument = None
-    try:
-        third_argument = msg_array[2]  # アニメ名 この引数は任意
-    except IndexError:
-        third_argument = None
-    if (first_argument == 'ANIMENTRODUCE'):
-        if (second_argument == '今期アニメ'):
-            year = datetime.now().year  # 今年の年を取得
-            course = ceil(datetime.now().month / 3) # 今クールの値を取得 例：1~3月なら1、4~6月なら2、7~9月なら3、10~12月なら4
-            data = callapi(year, course)
-            if (third_argument != None):
-                    if(data[i]['title'] == msg_array[2]):
-                        message = data[i]['title'] + '\n' + data[i]['public_url'] + '\n#' + data[i]['twitter_hash_tag']
-                    else:
-                        message = 'そのアニメは今期に放送されていないよ。'
-            else:
-                message = ''
-                for i in range(len(data)):
-                    message += data[i]['title'] + '\n'
-        elif (second_argument == '来期アニメ'):
-            year, course = checkcourse(datetime.now().year, ceil(datetime.now().month / 3)+1)
-            data = callapi(year, course)
-            if (third_argument != None):
-                for i in range(len(data)):
-                    if(data[i]['title'] == msg_array[2]):
-                        message = data[i]['title'] + '\n' + data[i]['public_url'] + '\n#' + data[i]['twitter_hash_tag']
-                    else :
-                        message = 'そのアニメは来期に放送されないよ。'
-            else:
-                message = ''
-                for i in range(len(data)):
-                    message += data[i]['title'] + '\n'
-        elif (second_argument == '前期アニメ'):
-            year, course = checkcourse(datetime.now().year, ceil(datetime.now().month / 3)-1)
-            data = callapi(year, course)
-            if (third_argument != None):
-                for i in range(len(data)):
-                    if(data[i]['title'] == msg_array[2]):
-                        message = data[i]['title'] + '\n' + data[i]['public_url'] + '\n#' + data[i]['twitter_hash_tag']
-                    else:
-                        message = 'そのアニメは前期に放送されていないよ。'
-            else:
-                message = ''
-                for i in range(len(data)):
-                    message += data[i]['title'] + '\n'
+
+    if (first_argument == '今期アニメ'):
+        year = datetime.now().year  # 今年の年を取得
+        course = ceil(datetime.now().month / 3) # 今クールの値を取得 例：1~3月なら1、4~6月なら2、7~9月なら3、10~12月なら4
+        data = callapi(year, course)
+        if (second_argument != None):
+            for i in range(len(data)):
+                if(data[i]['title'] == second_argument):
+                    message = data[i]['title'] + '\n' + data[i]['public_url'] + '\n' + str(year) + "年" +cool[course]
         else:
-            message = '不正操作しようとしてない？'
+            message = ''
+            for i in range(len(data)):
+                message += data[i]['title'] + '\n'
+    elif (first_argument == '来期アニメ'):
+        year, course = checkcourse(datetime.now().year, ceil(datetime.now().month / 3)+1)
+        data = callapi(year, course)
+        if (second_argument != None):
+            for i in range(len(data)):
+                if(data[i]['title'] == second_argument):
+                    message = data[i]['title'] + '\n' + data[i]['public_url'] + '\n' + str(year) + "年" +cool[course]
+        else:
+            message = ''
+            for i in range(len(data)):
+                message += data[i]['title'] + '\n'
+    elif (first_argument == '前期アニメ'):
+        year, course = checkcourse(datetime.now().year, ceil(datetime.now().month / 3)-1)
+        data = callapi(year, course)
+        if (second_argument != None):
+            for i in range(len(data)):
+                if(data[i]['title'] == second_argument):
+                    message = data[i]['title'] + '\n' + data[i]['public_url'] + '\n' + str(year) + "年" +cool[course]
+        else:
+            message = ''
+            for i in range(len(data)):
+                message += data[i]['title'] + '\n'
     elif (first_argument == 'ヘルプ'):
         message = 'このBotはアニメの情報を提供するよ。ボタンの使い方を説明するね。\n' + '『今期アニメ』\n『来期アニメ』\n『前期アニメ』\nそれぞれの時期に合ったアニメ情報が得られるよ。\n' + '『ヘルプ』\nこのメッセージを表示できるよ。'
     else:
-        message = 'なんか君おかしいよ。'
+        message = 'なんか君不正操作しようとしてない？'
 
     test_message = [
                 {
